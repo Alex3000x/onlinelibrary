9ca57d67a7e875fbb71209dbe1d018e6ddf8b798
Elisa Plessi <e.plessi@itsolivetti.it>
2026-04-29 00:10:08 +0200

    add database connection utility for MongoDB

---
+++ server/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
+
+let db;
+
+export default async function connectDB() {
+    if (!db) {
+        await client.connect();
+
+        console.log("Connected to MongoDB");
+
+        db = client.db("librarydb");
+    }
+    return db;
+}
