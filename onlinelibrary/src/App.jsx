9ec64fa8754edc2223f954516715f1e1ff1781a3
Elisa Plessi <e.plessi@itsolivetti.it>
2026-04-29 00:03:19 +0200

    refactor App component structure and update layout with Bootstrap

--- onlinelibrary/src/App.jsx
+++ onlinelibrary/src/App.jsx
@@ -1,14 +1,42 @@
-function App() {
+import {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Books from './components/Books';
+
+
+function App() 
+{
+  console.log("APP()");
+
+
+  // // funzione per aggiornare array dei commenti
+  // function addComment(comment) {
+  //   //comments.push(comment) // NOOOOOO!!!! IN REACT!! comments variabile di stato, renderizzazione componente 
+  //   setComments((cs) => [...cs,comment]);  
+  // }
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <h1>BIBLIOTECAAAAAA!</h1>
-        </div>
-      </section>
-    </>
+    <Container>
+      <h1>BIBLIOTECCCAHHH!!11!!!</h1>
+      
+      <Books />
+
+    </Container>
+    // <> </> fragment - container fake, mi serve perchè se no mi da errore tutorial, non è però un vero contenitore aggiuntivo
+    // per far passare lista di elementi senza creare per forza un contenitore HTML
+    //  <Tutorial tutorialP={tutorials[0]} /> sto passando oggetto che andrò a riconoscere tramite TutorialP
+    // devo mettere le graffe perchè sto passando un oggetto JS - inietto info JS e quindi per forza graffe
   )
 }
 
-export default App
+export default App;
+
+// definisco percorso per recuperare immagine - associo immagine a identificativo (che uso poi nell'array)
+// ./ -> percorso relativo, parte da cartella corrente in cui si trova file, comincia da directory in cui c'è App.jsx
+// sistema crea un puntatore a quell'immagine e lo salva dentro un nome che definiamo noi al momento -> reactImg
+
+//App rimane molto pulita, perchè abbiamo delegato tutorials e gestire tutte le problematiche, compresa 
+// parte di allineamento e distribuzione
