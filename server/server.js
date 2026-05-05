b9f9996b6d38ed825df5116adb2c49b8ef155528
Alessio Gurgoglione <alessio.gurgoglione@gmail.com>
2026-05-05 20:17:32 +0200

    implement main filtering by params for book retrieval in online library API (and other types of filtering retrieval just in case)

--- server/server.js
+++ server/server.js
@@ -10,23 +10,69 @@ app.use(cors()); // abilita CORS per tutte le rotte
 
 let db; 
 
-// GET /library - Retrieve all books, no filter
-app.get("/library", async (req, res, next) => {
+// GET /onlinelibrary/books?property= - Retrieve books by title, author, publication year, genre, ISBN, publisher, or language
+app.get("/onlinelibrary/books", async (req, res, next) => {
     try {
         const booksCollection = db.collection("books");
         const filter = {};
 
-        // // name filter (multiple values)
-        // if (req.query.name) {
-        //     const names = req.query.name.split(",");
-        //     filter.name = { $in: names};
-        // }
+        // case tolerant filter by title (single value)
+        if (req.query.title) {
+            filter.title = {
+                $regex: req.query.title.trim(),
+                $options: "i" // case insensitive
+            };
+        }
 
-        // // Magnitude greater than threshold
-        // if(req.query.minMagnitude) {
-        //     filter.magnitude = { $gt: Number(req.query.minMagnitude) };
-        // }
-        // console.log(filter);
+        // case tolerant filter by author (single value)
+        if (req.query.author) {
+            filter.author = {
+                $regex: request.query.author.trim(),
+                $options: "i"
+            };
+        }
+
+        // case tolerant filter by publicationYear (single value)
+        if (req.query.publicationYear) {
+            filter.publicationYear = {
+                $regex: req.query.publicationYear.trim(),
+                $options: "i"
+            };
+        }
+
+        // case tolerant filter by genre (single value)
+        if (req.query.genre) {
+            filter.genre = {
+                $regex: req.query.genre.trim(),
+                $options: "i"
+            };
+        }
+
+        // case tolerant filter by ISBN (single value)
+        if (req.query.ISBN) {
+            filter.ISBN = {
+                $regex: req.query.ISBN.trim(),
+                $options: "i"
+            };
+        }
+
+        // case tolerant filter by publisher (single value)
+        if (req.query.publisher) {
+            filter.publisher = {
+                $regex: req.query.publisher.trim(),
+                $options: "i"
+            };
+        }
+
+        // case tolerant filter by language (single value)
+        if (req.query.language) {
+            filter.language = {
+                $regex: req.query.language.trim(),
+                $options: "i"
+            };
+        }
+
+        console.log(filter);
         const onlinelibrary = await booksCollection.find(filter).toArray();
         res.json(onlinelibrary);
     }
@@ -34,6 +80,50 @@ app.get("/library", async (req, res, next) => {
     next(err);
     }});
 
+// 2 Esempio: GET /online/books/1984 - Retrieve a book by its code 
+app.get("/online/books/:name", async (req, res, next) => {
+    try {
+        const booksCollection = db.collection("books");
+
+        // // 1. case sensitive
+        // const filter = {
+        //     name: req.params.name
+        // }; // filtro per nome
+
+        //2. case tolerant
+        const filter = {
+            name: {
+                $regex: `^${req.params.name}$`,
+                $options: "i" // case insensitive
+            }
+        };
+
+        const book = await booksCollection.find(filter).toArray();
+        res.json(book);
+    } catch (err) {
+        next(err);
+    }});
+
+// GET /online/books/1 - Retrieve a book by its code
+app.get("/online/books/:code", async (req, res, next) => {
+    try {
+        const booksCollection = db.collection("books");
+
+        const filter = {
+            code: parseInt(req.params.code)
+        };
+
+        const book = await booksCollection.findOne(filter);
+
+        console.log(book);
+
+        //res.status(200).json(stars);
+        res.json(book);
+
+    } catch (err) {
+        next(err);
+    }
+});
 
 async function startServer() {
     try{
